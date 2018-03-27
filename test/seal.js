12077dbbfcc24075f8c7ad29fe1ce01290878057
yussan <yussandeveloper@gmail.com>
2018-03-27 10:41:15 +0700

    test

--- test/seal.js
+++ test/seal.js
@@ -1,11 +1,15 @@
 import Seal from '../src'
-// test validation in 2 seconds
-const seal = new Seal('my-app-key-123456', 2000)
+const SealClass = new Seal('my-app-key-123456', 10)
+const seal = SealClass.generateSeal()
 
-it('will passed', (done) => {
-  done()
-})
-
-it('will failed, because limit is 2 seconds', (done) => {
-  done()
+describe('seal test', () => {
+  it('will passed', (done) => {
+    if(SealClass.validate(seal).is_valid) done()
+  })
+  
+  it('will failed, because limit is 1 seconds', (done) => {
+    return new Promise(resolve => {
+      if(!SealClass.validate(seal).is_valid) done()
+    })
+  }).timeout(1000)
 })
