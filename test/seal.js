a3e7da2969b2cf14770d3849f3ec62400e90efd8
yussan <yussandeveloper@gmail.com>
2018-03-27 10:57:25 +0700

    stable

--- test/seal.js
+++ test/seal.js
@@ -1,5 +1,13 @@
+/**
+ * @author Yusuf A.H.
+ * @email yussandeveloper@gmail.com
+ * @create date 2018-03-27 10:50:08
+ * @modify date 2018-03-27 10:50:08
+ * @desc [description]
+*/
 import Seal from '../src'
-const SealClass = new Seal('my-app-key-123456', 10)
+// seal will valid in next 500ms
+const SealClass = new Seal('my-app-key-123456', 500)
 const seal = SealClass.generateSeal()
 
 describe('seal test', () => {
@@ -8,8 +16,8 @@ describe('seal test', () => {
   })
   
   it('will failed, because limit is 1 seconds', (done) => {
-    return new Promise(resolve => {
-      if(!SealClass.validate(seal).is_valid) done()
-    })
-  }).timeout(1000)
+    setTimeout(() => {
+      if(SealClass.validate(seal).is_valid === false) done() 
+    }, 1000)
+  })
 })
